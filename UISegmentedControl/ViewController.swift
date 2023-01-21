f52b9906f9188524e2e6322bef0a0c076a1aaa12
Manas Duishoev <manasduishoev@gmail.com>
2023-01-21 23:26:35 +0600

    Initial Commit

---
+++ UISegmentedControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISegmentedControl
+//
+//  Created by new on 21/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
